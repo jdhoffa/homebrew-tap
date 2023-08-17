4a7aae634c98959b0cfec1c714afb5f47ebec859
Jackson Hoffart <jackson.hoffart@gmail.com>
2023-08-17 07:52:17 -0700

    add shasum for pacta-cli

--- Formula/pacta-cli.rb
+++ Formula/pacta-cli.rb
@@ -1,8 +1,8 @@
-class Yourapp < Formula
+class PactaCli < Formula
   desc "A CLI for PACTA for Investors"
   homepage "https://github.com/jdhoffa/pacta-cli"
   url "https://github.com/jdhoffa/pacta-cli/archive/refs/tags/v0.0.0.9000.tar.gz"
-  sha256 "..."
+  sha256 "57f92f65028b56b6eedf989a3743b0f7a565ea3a50047019779a0cfb94659b55"
 
   depends_on "rust" => :build
   depends_on "docker" => :build
