37bb0aac900550f160900061183eb8a7f42d2918
Jackson Hoffart <jackson.hoffart@gmail.com>
2023-08-17 09:20:32 -0700

    bump pacta-cli release

--- Formula/pacta-cli.rb
+++ Formula/pacta-cli.rb
@@ -1,8 +1,8 @@
 class PactaCli < Formula
   desc "A CLI for PACTA for Investors"
   homepage "https://github.com/jdhoffa/pacta-cli"
-  url "https://github.com/jdhoffa/pacta-cli/archive/refs/tags/v0.0.0.9000.tar.gz"
-  sha256 "5809c38a2291aea166dc0a984861b4f2636088a280156901e53bd57aafd5d9b9"
+  url "https://github.com/jdhoffa/pacta-cli/archive/refs/tags/v0.0.1.tar.gz"
+  sha256 "78c8d6d8bcf3616f201fa6eff0d543c759bfa806a78f59e5846a0181ec197689"
 
   depends_on "rust" => :build
   depends_on "docker" => :build
